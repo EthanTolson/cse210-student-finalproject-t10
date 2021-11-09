66f29910b95e34c53221d3e99fc2bc18d8b151ee
Ethan Tolson <tol18009@byui.edu>
2021-11-08 18:41:40 -0800

    Fixed constants so that you can use resources when you run main nomatter what the base file path is

--- project/game/constants.py
+++ project/game/constants.py
@@ -1,5 +1,7 @@
+import os
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCALING = 1/8
-RESOURCE_PATH = "cse210-student-finalproject-t10/project/game/resources/"
+RESOURCE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/resources/" #cse210-student-finalproject-t10/project/game/
 SCREEN_TITLE = "Enter's Game"
